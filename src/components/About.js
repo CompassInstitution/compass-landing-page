d05cba2d6316db649534d99e52bf9a0175a8b335
MatteoPerona <matteo.cmp.personal@gmail.com>
2022-12-31 15:35:17 -0800

    basic formatting for navbar and about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,22 @@
 // Matteo
 export default function About() {
     return (
-        <div className="sticky font-serif font-extrabold">
-            <h1>
-                compass compass compass
-            </h1>
+        <div className="flex items-center container  pt-5">
+            <div className="sticky font-serif ">
+                <h1 className="font-extrabold text-3xl">
+                    Mathematical Modeling for Social Good
+                </h1>
+                <h2 className="text-3xl">
+                    @ UC San Diego
+                </h2>
+                
+                <p className="text-lg">
+                    We're a student run organization partnering with local and international organizations to amplify their impact through mathematics, computer science, and data science.
+                </p>
+            </div>
+            <div className="basis-1/2">
+
+            </div>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 export default function Navbar() {
     return (
         <nav className="sticky top-0 w-full border border-b-1 z-50">
-            <div className="container max-w-5xl">
+            <div className="container">
                 <div className="flex flex-row py-1 items-center font-serif">
                     <div className="basis-1/3">
-                        logo
+                        <img src="logo192.png" alt="Logo" width="90" />
                     </div>
                     <button className="basis-1/6 basis-">
                         about
@@ -18,8 +18,12 @@ export default function Navbar() {
                     <button className="basis-1/6">
                         jobs
                     </button>
-                    <button className="basis-1/3">
-                        get involved
+                    <button className="basis-1/6 flex items-center rounded ">
+                        <div className="w-9">
+                        </div>
+                        <div className="rounded ring-offset-2 ring-2 ring-black w-28 bg-slate-100">
+                            get involved
+                        </div>
                     </button>
                 </div>
             </div>
