c0260f575505295cfafcc9401acc3965952a7efe
MatteoPerona <matteo.cmp.personal@gmail.com>
2023-01-08 15:13:09 -0800

    added template card

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+export default function Events({ title, date, description, rsvpLink, itemId }) {
+    return (
+        <div className="px-3">
+            <div className="container items-stretch py-3 rounded-3xl bg-gradient-to-tr from-gray-100 to-sky-100 w-96">
+                <div className="font-sans" itemID={itemId} key={itemId}>
+                    <h1 className=" font-semibold text-2xl">{title}</h1>
+                    <div>{description}</div>
+                    <div>{date}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/Events.js
+++ src/components/Events.js
@@ -1,62 +1,62 @@
 // Reese
 import { ScrollMenu } from "react-horizontal-scrolling-menu";
+import Card from './Card';
+
 
 const events = [];
 
 export default function Events() {
-  return (
-    <div className="flex items-center container  pt-5">
-      <div div className="sticky font-serif ">
-        <h1 className="font-extrabold text-3xl">Events</h1>
-        <div>
-          <ScrollMenu>{events.map((event) => EventCard(event))}</ScrollMenu>
+    return (
+        <div className="items-center container text-center pt-5">
+            <h1 className="font-semibold text-2xl ">Upcoming Events</h1>
+            <div div className="sticky font-sans items-center">
+                <ScrollMenu>{events.map((event) => Card(event))}</ScrollMenu>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-function EventCard({ title, date, description, rsvpLink, itemId }) {
-  return (
-    <div
-      style={{
-        border: "1px solid",
-        margin: "0 10px",
-        width: "200px",
-      }}
-      itemID={itemId}
-      key={itemId}
-    >
-      <div>
-        <div>{date}</div>
-        <div>{title}</div>
-      </div>
-      <div>
-        <div>{description}</div>
-      </div>
-      <div>
-        <a href={rsvpLink}>{rsvpLink}</a>
-      </div>
-    </div>
-  );
-}
+// function EventCard({ title, date, description, rsvpLink, itemId }) {
+//     return (
+//         <div
+//             style={{
+//                 border: "1px solid",
+//                 margin: "0 10px",
+//                 width: "200px",
+//             }}
+//             itemID={itemId}
+//             key={itemId}
+//         >
+//             <div>
+//                 <div>{date}</div>
+//                 <div>{title}</div>
+//             </div>
+//             <div>
+//                 <div>{description}</div>
+//             </div>
+//             <div>
+//                 <a href={rsvpLink}>{rsvpLink}</a>
+//             </div>
+//         </div>
+//     );
+// }
 
 const exampleEvent = {
-  title: "Example",
-  date: "1/9/2022",
-  description: "Attend our example event!",
-  rsvpLink: "www.example.com",
-  itemId: "exEvId",
+    title: "Example",
+    date: "1/9/2022",
+    description: "Attend our example event!",
+    rsvpLink: "www.example.com",
+    itemId: "exEvId",
 };
 
 events.push(exampleEvent);
 
 const exampleEvent2 = {
-  title: "Example2",
-  date: "1/10/2022",
-  description: "Attend this second example event!",
-  rsvpLink: "www.example2.com",
-  itemID: "exEv2Id",
+    title: "Example2",
+    date: "1/10/2022",
+    description: "Attend this second example event!",
+    rsvpLink: "www.example2.com",
+    itemID: "exEv2Id",
 };
 
 events.push(exampleEvent2);
